7c055e1c806ea0a00de3388051acaba6f55d2237
Aasem Hany <assemsoma541@gmail.com>
2022-10-23 10:37:32 +0200

    Populate the app with an AlertActionSheet to make the user Choose from a list of websites to load

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -21,11 +21,45 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let url = URL(string: "https://www.hackingwithswift.com/100")!
+        configureNavBar()
+        
+        loadWebsite(ofUrl: "https://www.hackingwithswift.com/100")
+    }
+    
+    
+    private func loadWebsite(ofUrl url:String) {
+        let url = URL(string: url)!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
+    private func configureNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain,  target: self, action: #selector(onOpenPressed))
+    }
+    
+    @objc private func onOpenPressed() {
+        let ac = UIAlertController(title: "Open", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Apple Websit", style: .default, handler: {(_) in
+            self.loadWebsite(ofUrl: "https://www.apple.com")
+        }))
+        
+        ac.addAction(UIAlertAction(title: "My Github account", style: .default, handler: {(_) in
+            self.loadWebsite(ofUrl: "https://github.com/assem16399")
 
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+
+        present(ac, animated: true)
+        
+    }
+
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
